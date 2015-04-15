1ef45b8276edf6df21f933185c717d29d46c2042
David Peppel <skeltonath@gmail.com>
2015-04-15 16:53:44 -0700

    Added logging for forever

--- src/app.js
+++ src/app.js
@@ -10,4 +10,8 @@ var server = app.listen(4000, function() {
   var port = server.address().port;
 
   console.log('App listening at http://%s:%s', host, port);
+
+  setInterval(function() {
+    console.log('still alive...')
+  }, 1000 * 30);
 });
